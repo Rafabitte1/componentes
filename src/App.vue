367eaf527511d861f32c245015c64450088acb81
Rafabitte1 <rafabitte1@gmail.com>
2024-07-15 15:07:01 -0300

    .

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import ExpandBox from './components/ExpandBox.vue';
+  const title = 'minha caixa'
+  const caixa = [
+{ id: 1 , title: 'caixa1', content: 'conteudo-1', quatity: 100},
+{id: 2, title: 'caixa 2'},
+{id: 3, title: 'caixa 3', quatity : 50},
+{id: 1, title: 'caixa 4', content: 'conteudo secreto 4'}
+  ]
+</script>
+
+<template>
+  <div>
+    <expand-box :title="title" />
+   <!---- <expand-box title="Título da caixa de expansão " content="segredo" />
+    <expand-box title="Título da caixa de expansão 2"  />
+    <expand-box title="Título da caixa de expansão 3" quantity="20"  /> --->
+  </div>
+</template>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,53 @@
+<script setup>
+  import { computed, ref, defineProps } from 'vue';
+
+  //const props = defineProps(['title', 'content' ]);
+
+   const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+
+content :{
+    type: String,
+    default : 'Valor padrão do texto'
+},
+
+quantity:{
+    type: Number,
+    default : '10'
+}
+
+  })
+
+
+  const showContent = ref(false);
+  const buttonText = computed(() =>
+    showContent.value ? 'Esconder' : 'Mostrar',
+  );
+</script>
+
+<template>
+  <div>
+    
+    <button @click="showContent=!showContent">{{buttonText}}</button>
+    <div v-if="showContent" class="expand-box">
+      <h1>{{ props.title }}</h1>
+      <p>Conteúdo da caixa de expansão.</p>
+      <p>{{ props.content }}</p>
+      <p>{{ quantity }}</p>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+  .expand-box {
+    height: 100%;
+    padding: 20px;
+    border: 1px solid gray;
+    border-radius: 5px;
+    box-shadow: 3px 3px 2px 2px gray;
+  }
+</style>
